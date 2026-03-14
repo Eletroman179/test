04816a36d6dfb87cf6c904c96b31bce5b5e008d3
Eletroman179 <jameschaowadee@gmail.com>
2026-03-14 13:25:52 +0700

    Update wdjadjs.py

--- wdjadjs.py
+++ wdjadjs.py
@@ -1,1 +1,10 @@
-print("hello")
+import socket
+
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
+print("I hacked into yocur pc i know your ip addr")
+print("it is\n")
+
+print("Your Computer Name is:", hostname)
+print("Your Computer IP Address is:", IPAddr)
